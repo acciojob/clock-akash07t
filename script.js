fe488dd641df0e8ee1357dd25e63347836fadd46
Akash Tiwari <124699923+akash07t@users.noreply.github.com>
2023-04-17 18:38:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 
 let clock = document.getElementById('timer');
 let info = new Date();
+let day = info.getDate();
+let month = info.getMonth()+1;
+let year = info.getFullYear();
+var time = info.getHours() + ":" + info.getMinutes() + ":" + info.getSeconds();
 
-clock.innerHTML = info;
+clock.innerHTML = `${day}/${month}/${year},${time}`;
 
